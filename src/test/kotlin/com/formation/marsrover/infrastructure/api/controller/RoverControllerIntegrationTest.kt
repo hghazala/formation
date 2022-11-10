a01e072300a12dd0d48bec9531918b1d95abbd87
jluch <jean-luc.uch@lafoncierenumerique.com>
2022-11-10 15:01:31 +0100

    should get rover from adapter (mockk)

--- src/test/kotlin/com/formation/marsrover/infrastructure/api/controller/RoverControllerIntegrationTest.kt
+++ src/test/kotlin/com/formation/marsrover/infrastructure/api/controller/RoverControllerIntegrationTest.kt
@@ -1,11 +1,15 @@
 package com.formation.marsrover.infrastructure.api.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.formation.marsrover.infrastructure.api.RoverAdapter
 import com.formation.marsrover.infrastructure.api.RoverView
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -18,11 +22,17 @@ class RoverControllerIntegrationTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @MockkBean
+    private lateinit var roverAdapter: RoverAdapter
+
     private val jsonMapper = ObjectMapper()
 
     @Test
     fun `should get rover`() {
         val rover = RoverView(0,0,"N")
+        every { roverAdapter.getRover() } returns rover
+
         mockMvc.perform(
             MockMvcRequestBuilders.get("/rover")
         ).andExpect(
